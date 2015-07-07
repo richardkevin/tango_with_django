9d1ee514451122c3963eb76ee216512a4c0e5bbf
Richard <richardkevin@gmail.com>
2015-07-07 12:37:18 -0300

    preparando para subida no heroku

--- tango_with_django_project/wsgi.py
+++ tango_with_django_project/wsgi.py
@@ -7,7 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
+from unipath import Path
 import os
+import sys
+
+
+BASE_DIR = Path(__file__).parent.parent
+if BASE_DIR not in sys.path:
+    sys.path.append(BASE_DIR)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tango_with_django_project.settings")
 
 from django.core.wsgi import get_wsgi_application
